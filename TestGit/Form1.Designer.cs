cb6c8ee6c1c83b745394738e29c50733b1c62d53
Sinzxc <sin.zxc@bk.ru>
2023-01-20 19:40:13 +0300

    Цвет

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -64,6 +64,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
             this.ClientSize = new System.Drawing.Size(326, 176);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
